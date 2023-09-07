14cfe7db36b964e97272495f328db78bc4200ca7
Mathías Wismann <mwbertolucci@gmail.com>
2023-09-07 11:58:00 -0400

    Add Solver class with first tdd method #factorial

---
+++ solver.rb
@@ -0,0 +1,7 @@
+class Solver
+  def factorial(num)
+    raise ArgumentError, 'Negative numbers are not allowed' if num.negative?
+    return 1 if num <= 1
+    (1..num).reduce(:*)
+  end
+end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,3 +1,5 @@
+require_relative '../solver'
+
 describe Solver do
   before(:each) do
     @solver = Solver.new
