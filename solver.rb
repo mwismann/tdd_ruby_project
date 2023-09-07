a110dc791b63e1445dd2dd91e6e6674231bfd67c
Reza Merzaie <ghrezam97@gmail.com>
2023-09-07 20:51:50 +0430

    Add the method for the previous tdd

--- solver.rb
+++ solver.rb
@@ -8,4 +8,12 @@ class Solver
   def reverse(str)
     str.reverse
   end
+
+  def fizzbuzz(number)
+    return 'fizzbuzz' if (number % 3).zero? && (number % 5).zero?
+    return 'fizz' if (number % 3).zero?
+    return 'buzz' if (number % 5).zero?
+
+    number.to_s
+  end
 end
