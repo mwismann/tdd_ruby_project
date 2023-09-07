ab39ba6fe7d8ba02d7ff44a45001699ad9053540
Mathías Wismann <mwbertolucci@gmail.com>
2023-09-07 11:53:24 -0400

    Add solver_spec.rb

---
+++ spec/solver_spec.rb
@@ -0,0 +1,22 @@
+describe Solver do
+  before(:each) do
+    @solver = Solver.new
+  end
+
+  describe '#factorial' do
+    it 'returns the factorial of a number' do
+      expect(@solver.factorial(5)).to eq 120
+      expect(@solver.factorial(6)).to eq 720
+      expect(@solver.factorial(7)).to eq 5040
+      expect(@solver.factorial(8)).to eq 40_320
+    end
+
+    it 'retunrs 1 when given 0' do
+      expect(@solver.factorial(0)).to eq 1
+    end
+
+    it 'returns an exception when given a negative number' do
+      expect { @solver.factorial(-1) }.to raise_error(ArgumentError)
+    end
+  end
+end
